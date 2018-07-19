8e9bf3de64d9a68dc92cf952c57abd1510953ea2
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-07-19 22:40:48 +0200

    script: gem hash reference for further manipulation

--- scripts/multiple_gem_versions.pl
+++ scripts/multiple_gem_versions.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Cwd qw(cwd);
+use File::Path qw( make_path remove_tree );
 
 # Directories
 my $root_path = cwd();
@@ -13,3 +14,19 @@ my $pkg_dir = "${root_path}/pkg";
 print $root_path . "\n";
 print $target_dir . "\n";
 print $pkg_dir . "\n";
+
+# https://rubygems.org/downloads/jsonschema_serializer-0.1.0.gem
+my $gem = {
+  source => "https://rubygems.org",
+  name => "jsonschema_serializer",
+  main_module => "JsonschemaSerializer",
+  versions => [qw( 0.0.5 0.1.0 )]
+};
+
+print $gem->{source} . "\n";
+print $gem->{name} . "\n";
+print $gem->{main_module} . "\n";
+print join(', ', @{$gem->{versions}}) . "\n";
+
+my $gemspec = $gem->{name} . ".gemspec";
+print $gemspec . "\n";
