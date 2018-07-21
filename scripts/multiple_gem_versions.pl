a98ae61480dfd27db9355e3328e69ffb687f66f6
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-07-21 11:53:28 +0200

    script: second version of fetch_gem

--- scripts/multiple_gem_versions.pl
+++ scripts/multiple_gem_versions.pl
@@ -49,19 +49,26 @@ sub unpack_gem {
 }
 
 sub fetch_gem {
-  my $gem_filepath = shift;
-  my $gem_version = shift;
-  if (! -e $gem_filepath ) {
+  my ( $gem_filepath, $gem_version, $gem_vname ) = (@_);
+  if (! -f $gem_filepath ) {
     my $cmd = "gem fetch " . $gem->{name} . " --version ${gem_version}" .
               " --source $gem->{source}";
     `$cmd`;
-    move( $gem->{name}. '-' . $gem_version . "gem", $gem_filepath );
+    move( "${gem_vname}.gem", $gem_filepath );
   }
 }
 
 sub main {
-  reset_dir $pkg_dir;
-  fetch_gem("$pkg_dir/jsonschema_serializer-0.0.5.gem", "0.0.5");
+  reset_dir $target_dir;
+
+  foreach my $v (@{$gem->{versions}}) {
+    my $gem_vname = join('-', ($gem->{name}, $v));
+    my $gem_path = "$pkg_dir/${gem_vname}.gem";
+    print $gem_path . "\n";
+
+    fetch_gem($gem_path, $v, $gem_vname);
+    unpack_gem($gem_path);
+  }
 }
 
 main;
