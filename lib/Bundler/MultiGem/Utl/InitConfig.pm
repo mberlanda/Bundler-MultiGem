c3be2f503a2b027c7afc33c6193b7a4a8836e162
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-09-09 16:37:25 +0200

    chore: adopted Hash merge and added some testing

--- lib/Bundler/MultiGem/Utl/InitConfig.pm
+++ lib/Bundler/MultiGem/Utl/InitConfig.pm
@@ -3,6 +3,26 @@ package Bundler::MultiGem::Utl::InitConfig {
   use strict;
   use warnings;
 
+  use Exporter qw(import);
+  our @EXPORT = qw();
+
+  use Storable qw(dclone);
+  use Hash::Merge qw(merge);
+
+=head1 NAME
+
+Bundler::MultiGem::Utl::InitConfig - The utility to install multiple versions of the same ruby gem
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+This module contains a default configuration for the package to work and the utility functions to manipulate it.
+
+=cut
+
   our $DEFAULT_CONFIGURATION = {
     'gem' => {
       'source' => 'https://rubygems.org',
@@ -20,5 +40,16 @@ package Bundler::MultiGem::Utl::InitConfig {
       'target' => 0
     }
   };
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2 merge_configuration
+
+=cut
+  sub merge_configuration {
+    my $custom_config = shift;
+    my $result = merge($custom_config, dclone($DEFAULT_CONFIGURATION));
+  }
 };
 1;
