680f313bc78fba752f6f8571e347c395a2c5c55a
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-10-31 15:57:33 +0100

    doc: lib/Bundler/MultiGem/Utl/InitConfig.pm

--- lib/Bundler/MultiGem/Utl/InitConfig.pm
+++ lib/Bundler/MultiGem/Utl/InitConfig.pm
@@ -17,11 +17,38 @@ Bundler::MultiGem::Utl::InitConfig - The utility to install multiple versions of
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
+
+=cut
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
 This module contains a default configuration for the package to work and the utility functions to manipulate it.
+
+=cut
+
+=head1 DEFAULT_CONFIGURATION
+
+Default configuration used to build the yaml configuration file:
+
+    our $DEFAULT_CONFIGURATION = {
+      'gem' => {
+        'source' => 'https://rubygems.org',
+        'name' => undef,
+        'main_module' => undef,
+        'versions' => [()]
+      },
+      'directories' => {
+        'root' => undef,
+        'pkg' => 'pkg',
+        'target' => 'versions'
+      },
+      'cache' => {
+        'pkg' => 1,
+        'target' => 0
+      }
+    };
 
 =cut
 
@@ -43,9 +70,11 @@ our $DEFAULT_CONFIGURATION = {
   }
 };
 
-=head1 SUBROUTINES/METHODS
+=head1 EXPORTS
 
 =head2 merge_configuration
+
+Wrapper for merging a custom configuration with C<$DEFAULT_CONFIGURATION>
 
 =cut
 sub merge_configuration {
@@ -54,9 +83,17 @@ sub merge_configuration {
   default_main_module($result);
 }
 
-=head2 merge_configuration
+=head2 default_main_module
+
+Assing a default C<main_module> name to a gem if not set
+
+    my $gem_config = $custom_config->{gem};
+    ruby_constantize($gem_config->{name});
+
+E.g. C<foo> -> C<Foo>, C<foo_bar> -> C<FooBar>, C<foo-bar> -> C<Foo::Bar>
 
 =cut
+
 sub default_main_module {
   my $custom_config = shift;
   my $gem_config = $custom_config->{gem};
@@ -68,7 +105,15 @@ sub default_main_module {
 
 =head2 ruby_constantize
 
+Format string as a ruby constant
+
+    ruby_constantize("foo"); # Foo
+    ruby_constantize("foo_bar"); # FooBar
+    ruby_constantize("foo-bar"); # Foo::Bar
+    ruby_constantize("foo-bar_baz"); # Foo::BarBaz
+
 =cut
+
 sub ruby_constantize {
   my $name = shift;
   for ($name) {
